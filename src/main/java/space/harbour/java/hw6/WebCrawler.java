9474db925d9f56427f14ab73ff47e426c9559964
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-16 18:44:37 +0700

    Create repo with file from class

---
+++ src/main/java/space/harbour/java/hw6/WebCrawler.java
@@ -0,0 +1,61 @@
+package space.harbour.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class WebCrawler {
+    static ConcurrentLinkedQueue<URL> toVisit = new ConcurrentLinkedQueue<>();
+    static CopyOnWriteArraySet<URL> alreadyVisited = new CopyOnWriteArraySet<>();
+
+    public static class UrlVisitor implements Runnable {
+        public static String getContentOfWebPage(URL url) {
+            final StringBuilder content = new StringBuilder();
+
+            try (InputStream is = url.openConnection().getInputStream();
+                 InputStreamReader in = new InputStreamReader(is, "UTF-8");
+                 BufferedReader br = new BufferedReader(in); ) {
+                String inputLine;
+                while ((inputLine = br.readLine()) != null)
+                    content.append(inputLine);
+            } catch (IOException e) {
+                System.out.println("Failed to retrieve content of " + url.toString());
+                e.printStackTrace();
+            }
+
+            return content.toString();
+        }
+
+        @Override
+        public void run() {
+            // get a URL from the head of toVisit queue
+            URL url = toVisit.poll();
+            // mark it as visited by adding to alreadyVisited set
+            alreadyVisited.add(url);
+
+            // get content of the web page
+            String content = getContentOfWebPage(url);
+            // Remove this line after debugging before you submit your HW
+            System.out.println(content);
+
+            // get URLs from the contect
+            URL[] urls = {};
+            for (URL newUrl: urls) {
+                if (!alreadyVisited.contains(newUrl) && !toVisit.contains(newUrl)) {
+                    toVisit.add(newUrl);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws MalformedURLException {
+        toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
+        new Thread(new UrlVisitor()).start();
+    }
+
+}
