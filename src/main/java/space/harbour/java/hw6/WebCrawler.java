f1f6a77cc4f7b13c49808bb8db94ad1f1ef8d1bf
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-17 14:58:18 +0700

    Add webcralwer

--- src/main/java/space/harbour/java/hw6/WebCrawler.java
+++ src/main/java/space/harbour/java/hw6/WebCrawler.java
@@ -1,83 +1,35 @@
-package space.harbour.java;
+package space.harbour.java.hw6;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArraySet;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class WebCrawler {
-    static ConcurrentLinkedQueue<URL> toVisit = new ConcurrentLinkedQueue<>();
-    static CopyOnWriteArraySet<URL> alreadyVisited = new CopyOnWriteArraySet<>();
+    public static void main(String[] args) throws Exception {
+        Buffer buffer = new Buffer();
+        buffer.putQueue(new URL("https://iabeteetmechante.github.io/symmetrical-bassoon/index.html"));
 
-    public static class UrlVisitor implements Runnable {
-        public static String getContentOfWebPage(URL url) {
-            final StringBuilder content = new StringBuilder();
+        //for IO intensive tasks we can have a high count of threads
+        ExecutorService service = Executors.newFixedThreadPool(100);
 
-            try (InputStream is = url.openConnection().getInputStream();
-                    InputStreamReader in = new InputStreamReader(is, "UTF-8");
-                    BufferedReader br = new BufferedReader(in); ) {
-                String inputLine;
-                while ((inputLine = br.readLine()) != null) {
-                    content.append(inputLine);
-                }
-            } catch (IOException e) {
-                System.out.println("Failed to retrieve content of " + url.toString());
-                e.printStackTrace();
-            }
+        Future<CopyOnWriteArraySet> future;
 
-            return content.toString();
+        //submit the tasks for execution
+        //for (int i = 0; i < 1000; i++) {
+        while (!buffer.toVisit.isEmpty()) {
+            future = service.submit(new VisitorUrl(buffer));
+            System.out.println(future.get());
         }
 
-        @Override
-        public void run() {
-            // get a URL from the head of toVisit queue
-            URL url = toVisit.poll();
-            // mark it as visited by adding to alreadyVisited set
-            alreadyVisited.add(url);
-
-            // get content of the web page
-            String content = getContentOfWebPage(url);
-            // Remove this line after debugging before you submit your HW
-            System.out.println(content);
-
-            //get URLs from the content
-            String regex =
-                    "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-            Pattern p = Pattern.compile(regex);
-            Matcher m = p.matcher(content);
-            while (m.find()) {
-                System.out.println(m.group());
-                try {
-                    URL myUrl = new URL(m.group());
-                    if (!alreadyVisited.contains(myUrl) && !toVisit.contains(myUrl)) {
-                        toVisit.add(myUrl);
-                    }
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
-            }
-
-            // get URLs from the contect
-            //URL[] urls = {};
-            //for (URL newUrl : urls) {
-            //    if (!alreadyVisited.contains(newUrl) && !toVisit.contains(newUrl)) {
-            //        toVisit.add(newUrl);
-            //    }
-            //}
-        }
+        service.shutdown();
     }
-
-    public static void main(String[] args) throws MalformedURLException {
-        toVisit.add(new URL("https://vasart.github.io/supreme-potato/"));
-        new Thread(new UrlVisitor()).start();
-    }
-
 }
+
+
+
+
+
+
+
---
+++ src/test/java/space/harbour/java/hw6/WebCrawlerTest.java
@@ -0,0 +1,29 @@
+package space.harbour.java.hw6;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class WebCrawlerTest extends TestCase
+{
+    private WebCrawler myWebcrawler;
+
+    @Before
+    public void createWebcrawler()
+    {
+        myWebcrawler = new WebCrawler();
+    }
+
+    @Test
+    public void myTest()
+    {
+
+    }
+}
