46a3028e6a0b0d8cb48d716981fa97ef3463621b
IAbeteEtMechante <pierre.machine.learning@gmail.com>
2020-11-17 08:08:48 +0700

    Add the regex to detect the URLs in the content

--- src/main/java/space/harbour/java/hw6/WebCrawler.java
+++ src/main/java/space/harbour/java/hw6/WebCrawler.java
@@ -8,6 +8,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebCrawler {
     static ConcurrentLinkedQueue<URL> toVisit = new ConcurrentLinkedQueue<>();
@@ -18,11 +20,12 @@ public class WebCrawler {
             final StringBuilder content = new StringBuilder();
 
             try (InputStream is = url.openConnection().getInputStream();
-                 InputStreamReader in = new InputStreamReader(is, "UTF-8");
-                 BufferedReader br = new BufferedReader(in); ) {
+                    InputStreamReader in = new InputStreamReader(is, "UTF-8");
+                    BufferedReader br = new BufferedReader(in); ) {
                 String inputLine;
-                while ((inputLine = br.readLine()) != null)
+                while ((inputLine = br.readLine()) != null) {
                     content.append(inputLine);
+                }
             } catch (IOException e) {
                 System.out.println("Failed to retrieve content of " + url.toString());
                 e.printStackTrace();
@@ -43,13 +46,32 @@ public class WebCrawler {
             // Remove this line after debugging before you submit your HW
             System.out.println(content);
 
-            // get URLs from the contect
-            URL[] urls = {};
-            for (URL newUrl: urls) {
-                if (!alreadyVisited.contains(newUrl) && !toVisit.contains(newUrl)) {
-                    toVisit.add(newUrl);
+            //get URLs from the content
+            String regex =
+                    "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+            Pattern p = Pattern.compile(regex);
+            Matcher m = p.matcher(content);
+            while (m.find()) {
+                System.out.println(m.group());
+                try {
+                    URL myUrl = new URL(m.group());
+                    if (!alreadyVisited.contains(myUrl) && !toVisit.contains(myUrl)) {
+                        toVisit.add(myUrl);
+                    }
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
                 }
+
             }
+
+            // get URLs from the contect
+            //URL[] urls = {};
+            //for (URL newUrl : urls) {
+            //    if (!alreadyVisited.contains(newUrl) && !toVisit.contains(newUrl)) {
+            //        toVisit.add(newUrl);
+            //    }
+            //}
         }
     }
 
